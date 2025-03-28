e31e3dae5ecd6900a8618ca24c0dc8126fce426c
Jade Chen <jadeeechen@gmail.com>
2025-03-28 11:14:27 -0700

    added tests, created document(), edited others

--- R/mark_says.R
+++ R/mark_says.R
@@ -1,12 +1,12 @@
 #' Return a Mark quote
 #'
 #' This function prints the well-known quote:
-#' *"So that's unknown, unknown, Delaware, unknown, unknown… That's a perfect score!"*
+#' *"So that's unknown, unknown, Delaware, unknown, unknown... That's a perfect score!"*
 #'
 #' @return A character string containing Mark's quote.
+#' @export
 #' @examples
 #' mark_says()
-#' @export
 mark_says <- function() {
-  "So that's unknown, unknown, Delaware, unknown, unknown… That's a perfect score!"
+  "So that's unknown, unknown, Delaware, unknown, unknown... That's a perfect score!"
 }
---
+++ tests/testthat/test-mark_says.R
@@ -0,0 +1,3 @@
+test_that("prints quote", {
+  expect_equal(mark_says(), "So that's unknown, unknown, Delaware, unknown, unknown... That's a perfect score!")
+})
