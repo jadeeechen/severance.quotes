f7c41ef15c951219f57aca42ff3df40bccea5a04
Jade Chen <jadeeechen@gmail.com>
2025-03-28 10:15:08 -0700

    created mark_says()

---
+++ R/mark_says.R
@@ -0,0 +1,12 @@
+#' Return a Mark quote
+#'
+#' This function prints the well-known quote:
+#' *"So that's unknown, unknown, Delaware, unknown, unknown… That's a perfect score!"*
+#'
+#' @return A character string containing Mark's quote.
+#' @examples
+#' mark_says()
+#' @export
+mark_says <- function() {
+  "So that's unknown, unknown, Delaware, unknown, unknown… That's a perfect score!"
+}
